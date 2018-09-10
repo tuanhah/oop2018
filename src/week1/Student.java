7bbb9dd171585dd9f5442a80e23b257c91dfb96e
bqcuongas <bqcuong2212@gmail.com>
2018-09-10 10:39:14 +0700

    init week1

---
+++ src/week1/Student.java
@@ -0,0 +1,38 @@
+package week1;
+
+public class Student {
+
+    // TODO: khai báo các thuộc tính cho Student
+
+    // TODO: khai báo các phương thức getter, setter cho Student
+
+    /**
+     * Constructor 1
+     */
+    Student() {
+        // TODO:
+    }
+
+    /**
+     * Constructor 2
+     * @param n
+     * @param sid
+     * @param em
+     */
+    Student(String n, String sid, String em) {
+        // TODO:
+    }
+
+    /**
+     * Constructor 3
+     * @param s
+     */
+    Student(Student s) {
+        // TODO:
+    }
+
+    String getInfo() {
+        // TODO:
+        return null; // xóa dòng này sau khi cài đặt
+    }
+}
---
+++ src/week1/StudentManagement.java
@@ -0,0 +1,23 @@
+package week1;
+
+public class StudentManagement {
+
+    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+
+    public boolean sameGroup(Student s1, Student s2) {
+        // TODO:
+        return false; // xóa dòng này sau khi cài đặt
+    }
+
+    void studentsByGroup() {
+        // TODO:
+    }
+
+    void removeStudent(String id) {
+        // TODO:
+    }
+
+    public static void main(String[] args) {
+        // TODO:
+    }
+}
