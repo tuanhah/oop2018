1f44f3e4eedcd6934a79f24b1a2d445ed1e1dac8
bqcuong <bqcuong2212@gmail.com>
2018-09-19 10:42:22 +0700

    add week2

---
+++ src/week2/task1/Task1.java
@@ -0,0 +1,14 @@
+package week2.task1;
+
+public class Task1 {
+
+    public static int gcd(int a, int b) {
+        // TODO: Tính ước chung lớn nhất của 2 số a, b
+        return 0;
+    }
+
+    public static int fibonacci(int n) {
+        // TODO: Tìm số fibonacci ở vị trí n
+        return 0;
+    }
+}
---
+++ src/week2/task2/Fraction.java
@@ -0,0 +1,30 @@
+package week2.task2;
+
+public class Fraction {
+
+    // TODO: khai báo các thuộc tính
+
+    public Fraction(int numerator, int denominator) {
+        // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+    }
+
+    public Fraction add(Fraction other) {
+        // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
+        return null;
+    }
+
+    public Fraction subtract(Fraction other) {
+        // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
+        return null;
+    }
+
+    public Fraction multiply(Fraction other) {
+        // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
+        return null;
+    }
+
+    public Fraction divide(Fraction other) {
+        // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
+        return null;
+    }
+}
