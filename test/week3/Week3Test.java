3e85ab60a67e78161a7571bebf42a0d38e013942
bqcuong <bqcuong2212@gmail.com>
2018-09-23 19:41:37 +0700

    update week3 exercises

---
+++ test/week3/Week3Test.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class Week3Test {
+
+    // TODO: Viết 5 testcase cho phương thức max()
+
+    // TODO: Viết 5 testcase cho phương thức minOfArray()
+
+    // TODO: Viết 5 testcase cho phương thức calculateBMI()
+}
