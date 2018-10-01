7d9258833da26e7b85300f981d0597354470f85e
bqcuong <bqcuong2212@gmail.com>
2018-10-01 21:06:24 +0700

    update week 4

---
+++ src/week4/task1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package bài làm cho Câu 1
+ * TODO: Khai báo các lớp (class) theo Câu 1 yêu cầu. 1 file chỉ khai báo 1 class
+ * TODO: Mô tả các quan hệ giữa các class và thêm thuộc tính, phương thức cho chúng (mỗi class ít nhất 2 phương thức, 2 thuộc tính)
+ * TODO: Viết comment đầy đủ cho class, phương thức. Xem hướng dẫn tại đây (http://dongdiemthuy.github.io/java/javadoc/2014/09/30/su-dung-javadoc-va-maven-de-tao-tai-lieu-du-an.html)
+ * @author cuong
+ * @version 0.1
+ */
+package week4.task1;
