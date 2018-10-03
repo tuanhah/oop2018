a5a276205c6a834c2bddff345b12417ce1429b7d
tuanhah <tuantsukuyomi@gmail.com>
2018-10-03 21:52:59 +0700

    doneweek3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,12 +4,19 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if (m >n) return m ;
+        return n;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        if (array.length == 0 || array.length > 100)  return 0;
+        int min = array[0];
+        if (array.length > 1 ) {
+            for (int i = 1; i <array.length;i++)
+                if (min >array[i])  min = array[i];
+        }
+        return  min;
     }
 
     /**
@@ -20,6 +27,16 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
+        if (height <= 0 ) return null;
+        double bmi = weight /(height *height);
+        if (bmi < 18.5) return "Thiếu cân";
+        if (18.5 <= bmi  && bmi<=22.99) return "Bình thường";
+        if (23<=bmi  && bmi<= 24.99) return "Thừa cân";
+        if (bmi > 25) return "Béo phì";
         return null;
     }
+    public static void main(String[] args){
+
+    }
+
 }
--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,44 @@
 package week3;
 
+import org.junit.Test;
+import org.junit.runner.*;
+import org.junit.runner.notification.*;
+import static org.junit.Assert.*;
 public class Week3Test {
 
     // TODO: Viết 5 testcase cho phương thức max()
+    @Test
+    public void testMax(){
+        assertEquals(6,Week3.max(5,6));
+        assertEquals(0,Week3.max(-5,0));
+        assertEquals(-9,Week3.max(-9,-100));
+        assertEquals(100,Week3.max(100,3));
+        assertEquals(32,Week3.max(32,5));
+    }
 
     // TODO: Viết 5 testcase cho phương thức minOfArray()
-
+    @Test
+    public void testMinOfArray(){
+        int[] arr = {0,1,2,3,4,5};
+        assertEquals(0,Week3.minOfArray(arr));
+        arr[1] = -6;
+        assertEquals(-6,Week3.minOfArray(arr));
+        arr[4] = -5;
+        assertEquals(-6,Week3.minOfArray(arr));
+        arr[4] = -6;
+        assertEquals(-6,Week3.minOfArray(arr));
+        arr[4] = -9;
+        assertEquals(-9,Week3.minOfArray(arr));
+    }
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
+    @Test
+    public void testCaculateBMI(){
+        assertEquals("Thiếu cân",Week3.calculateBMI(10,2));
+        assertEquals("Thừa cân",Week3.calculateBMI(55,1.5));
+        assertEquals("Bình thường",Week3.calculateBMI(50,1.5));
+        assertEquals("Béo phì",Week3.calculateBMI(100,1.4));
+        assertEquals("Béo phì",Week3.calculateBMI(90,1.3));
+
+    }
+
 }
