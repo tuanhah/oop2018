5ae846a138ceb19bebf4b30253e01fd50f169e7a
bqcuong <bqcuong2212@gmail.com>
2018-10-08 12:56:01 +0700

    update week 6 exercise

--- src/week5/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package làm bài tập tuần 5
- * Đọc yêu cầu đề bài ở week5.pdf
- * Chương trình mẫu: https://youtu.be/RfG2hQN3zfM
- */
-package week5;
---
+++ src/week5_6/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package làm bài tập tuần 5 và tuần 6
+ * Đọc yêu cầu đề bài ở week5_6.pdf
+ * Chương trình mẫu: https://youtu.be/RfG2hQN3zfM
+ */
+package week5_6;
